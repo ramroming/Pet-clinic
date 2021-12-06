f39f53230e9aafe86093e0c13ce5db0d9bafba4a
lagger94hub <ramisaadaldden@gmail.com>
2021-12-06 23:51:58 +0300

    slider react

--- website/pet-clinic-app/src/components/pages/home/slider/Slider.js
+++ website/pet-clinic-app/src/components/pages/home/slider/Slider.js
@@ -1,32 +1,96 @@
 import react from "react"
+import { useState, useEffect, useRef } from "react"
+
 
 
 const Slider = () => {
-    return (
-        <>
-            <div className="slider-container flex-col gap-16p falign-center">
-                <img src="/media/imgs/right.png" className=" right"></img>
-                <img src="/media/imgs/left.png" className=" left "></img>
-                <div className="slide-content flex-col falign-center gap-24p">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, harum.</p>
-                    <h1>Lorem ipsum dolor sit amet.</h1>
-                    <a href="" className="btn-rec-purple">
-                        I'm a button
-                    </a>
-                    <img className="slider-img-desktop" src="/media/imgs/appointment.jpg" alt="app-image"/>
-                    <img className="slider-img-mobile" src="/media/imgs/vertical-appointment.jpg" alt="app-image"/>
-                </div>
+
+  const [slider, setSlider] = useState([true, false, false, false])
+  const position = useRef([0, 0]);
 
 
-            </div>
-            <div className="pag flex-row gap-8p fjust-center">
-                <i className=" fas fa-dot-circle active "></i>
-                <i className="fas fa-dot-circle"></i>
-                <i className="fas fa-dot-circle"></i>
-                <i className="fas fa-dot-circle"></i>
-            </div>
-        </>
-    )
+  const moveSlider = (event) => {
+    console.log('moveSlider')
+
+    if (event.target.className == 'right') {
+      console.log('right pressed')
+      if (position.current[1] + 1 != slider.length) {
+        position.current[0] = position.current[1]
+        position.current[1] = position.current[1] + 1
+        setSlider(() => {
+          return slider.map((value, index) => {
+            return (index === position.current[0] || index == position.current[1]) ? !value : value
+          })
+        })
+      }
+
+    } else {
+      if (position.current[1] - 1 >= 0) {
+        position.current[0] = position.current[1]
+        position.current[1] = position.current[1] - 1
+        setSlider(() => {
+          return slider.map((value, index) => {
+            return (index === position.current[0] || index == position.current[1]) ? !value : value
+          })
+        })
+      }
+    }
+  }
+  const pagSlide = (event) => {
+    const keyIndex = event.target.className.search('k')
+    const pagValue = event.target.className[keyIndex + 1]
+    position.current[0] = position.current[1]
+    position.current[1] = Number(pagValue)
+    setSlider(() => {
+      return slider.map((value, index) => {
+        return (index === position.current[0] || index == position.current[1]) ? !value : value
+      })
+    })
+  }
+
+
+  return (
+    <>
+      <div className="slider-container flex-col gap-16p falign-center">
+        <img src="/media/imgs/right.png" className="right"
+          onClick={(event) => moveSlider(event)}></img>
+        <img src="/media/imgs/left.png" className="left"
+          onClick={(event) => moveSlider(event)}></img>
+        {slider[0] && <div className="slide-content flex-col falign-center gap-24p">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, harum.</p>
+          <h1>Lorem ipsum dolor sit amet.</h1>
+          <a href="" className="btn-rec-purple">
+            I'm a button
+          </a>
+          <img className="slider-img-desktop" src="/media/imgs/appointment.jpg" alt="app-image" />
+          <img className="slider-img-mobile" src="/media/imgs/vertical-appointment.jpg" alt="app-image" />
+        </div>
+        }
+        {
+          slider[1] &&
+          <div className="slide-content flex-col falign-center gap-24p">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, harum.</p>
+            <h1>Lorem ipsum dolor sit amet.</h1>
+            <a href="" className="btn-rec-purple">
+              I'm a kooko
+            </a>
+            <img className="slider-img-desktop" src="/media/imgs/appointment.jpg" alt="app-image" />
+            <img className="slider-img-mobile" src="/media/imgs/vertical-appointment.jpg" alt="app-image" />
+          </div>
+
+        }
+
+
+
+      </div>
+      <div className="pag flex-row gap-8p fjust-center">
+        <i onClick={(event) => pagSlide(event)} className={slider[0] == true ?"fas k0 fa-dot-circle active ": "fas k0 fa-dot-circle"}></i>
+        <i onClick={(event) => pagSlide(event)} className={slider[1] == true ?"fas k1 fa-dot-circle active ": "fas k1 fa-dot-circle"}></i>
+        <i onClick={(event) => pagSlide(event)} className={slider[2] == true ?"fas k2 fa-dot-circle active ": "fas k2 fa-dot-circle"}></i>
+        <i onClick={(event) => pagSlide(event)} className={slider[3] == true ?"fas k3 fa-dot-circle active ": "fas k3 fa-dot-circle"}></i>
+      </div>
+    </>
+  )
 }
 
 export default Slider
