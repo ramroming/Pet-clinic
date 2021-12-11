6b309a30f94da1627638ec30a9bed912e269c0b2
lagger94hub <ramisaadaldden@gmail.com>
2021-12-11 14:34:17 +0300

    Add comments to Slider.js

--- website/pet-clinic-app/src/components/pages/home/slider/sliderAnimation.js
+++ website/pet-clinic-app/src/components/pages/home/slider/sliderAnimation.js
@@ -1,33 +1,29 @@
+// Each component my have one or more animations for each animation we difine an object that contains:
+// 1-initial property which tells the initial state of element at the begining of the animation
+// 2-final property which tells the final state of the element at the end of the animation
+// 3-exit property which tells the exit state of the element when it is removed from the VDOM
+// both final and exit properties has a transition property as well to give more detailed info about the animation
+// Note: initial, final and exit properties' names are arbitrary
 const contentSlider = {
-    initial: { 
-        x: '97vw',
-        y: 0,
-        // zIndex: 3,
-    },
-    final: {
-      x: 0,
-      transition: {
-        duration: 0.5
-      }
-    },
-    exit: {
-      x:'-97vw',
-    //   zIndex: 1,
-      transition: {
-        duration: 0.5
-      }
-    },
-    
-  }
-  
-//   const burgerItem = {
-//     initial: { y: 20, opacity: 0 },
-//     final: {
-//       y: 0,
-//       opacity: 1
-//     }
-//   }
-  
-  
-  export { contentSlider };
-  
+  initial: {
+    x: '97vw',
+    y: 0,
+  },
+  final: {
+    x: 0,
+    transition: {
+      duration: 0.5
+    }
+  },
+  exit: {
+    x: '-97vw',
+    transition: {
+      duration: 0.5
+    }
+  },
+
+}
+
+
+
+export { contentSlider };
