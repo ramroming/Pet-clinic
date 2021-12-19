f4bd950381998896365e6c3ce8c68f37bc97b13c
lagger94hub <ramisaadaldden@gmail.com>
2021-12-20 01:45:08 +0300

    finish appointments page

--- website/pet-clinic-app/src/components/pages/appointment/Appointment.js
+++ website/pet-clinic-app/src/components/pages/appointment/Appointment.js
@@ -1,14 +1,22 @@
 import ViewAppointmentSlider from "./appointmentSlider/ViewAppointmentSlider";
 import MakeAppointmentSlider from './makeAppointment/MakeAppointmentSlider'
+
 const Appointment = () => {
   return (
     <>
-      <div className="home-container flex-col gap-24p">
+      <div className="home-container flex-col  gap-24p">
         <div className="make-appointment-wrapper">
           <MakeAppointmentSlider />
         </div>
-        <div className="view-appointment-wrapper">
-          <ViewAppointmentSlider />
+        <div className="view-appointment-wrapper flex-row falign-center fjust-start gap-36p">
+
+          <div className="view-appointment-wrapper ref">
+            <ViewAppointmentSlider  status='active' />
+          </div>
+
+          <div className="view-appointment-wrapper ref">
+            <ViewAppointmentSlider status='past' />
+          </div>
         </div>
 
       </div>
