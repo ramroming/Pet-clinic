3676c656dac8f6bcb4bac680e6d107ee7ddcc768
ramroming <reemalhalbouni@hotmail.com>
2021-12-25 20:58:22 +0300

    fixing design

--- website/pet-clinic-app/src/components/pages/adoptionAd/AdoptionAd.js
+++ website/pet-clinic-app/src/components/pages/adoptionAd/AdoptionAd.js
@@ -4,46 +4,51 @@ const AdoptionAd = () => {
     return (
         <div className=" adoption-ad-wrapper home-container flex-col falign-center gap-24p ">
 
-            {/* first main flex item styled as a grid 
-            ++ pet intro, info and photo ++ */}
-            <div className="pet-basic-info-wrapper">
-
-                {/* first mini flex */}
-
-                <div className="intro flex-col falign-center fjust-center gap-16p">
-                    <p className="intro-hi">hi i'm</p>
-                    <p className="pet-name">Mimo</p>
-                    <p className="pet-breed">Domestic Shorthair</p>
-                </div>
+            {/* first main flex item ++ pet intro, info and photo ++ */}
+            <div className="pet-basic-info-wrapper flex-col falign-center fjust-center gap-24p">
 
                 {/* second mini flex */}
-                <div className="pet-image-container">
-                <img src="media/imgs/petphoto.jpg" alt='pet'></img>
+
+                <div className=" flex-col falign-center fjust-center gap-16p">
+
+                    <div className="intro flex-col falign-center fjust-center gap-16p">
+                        <p className="intro-hi">hi i'm</p>
+                        <p className="pet-name">Mimo</p>
+                        <p className="pet-breed">Domestic Shorthair</p>
+                    </div>
+
+                    {/* third mini flex */}
+                    <div className="pet-info flex-col gap-16p">
+
+                        <div className="flex-row gap-8p">
+                            <label>Age: </label>
+                            <p>2 Months</p>
+                        </div>
+
+                        <div className="flex-row gap-8p">
+                            <label>Gender: </label>
+                            <p>Female</p>
+                        </div>
+
+                        <div className="flex-row gap-8p">
+                            <label>Color: </label>
+                            <p>mixed</p>
+                        </div>
+
+                        <div className="flex-row gap-8p">
+                            <label>Located with: </label>
+                            <p>Shelter</p>
+                        </div>
+
+                    </div>
+
+
                 </div>
 
-                {/* third mini flex */}
-                <div className="pet-info flex-col gap-16p">
-
-                    <div className="flex-row gap-8p">
-                        <label>Age: </label>
-                        <p>2 Months</p>
-                    </div>
-
-                    <div className="flex-row gap-8p">
-                        <label>Gender: </label>
-                        <p>Female</p>
-                    </div>
-
-                    <div className="flex-row gap-8p">
-                        <label>Color: </label>
-                        <p>mixed</p>
-                    </div>
-
-                    <div className="flex-row gap-8p">
-                        <label>Located with: </label>
-                        <p>Shelter</p>
-                    </div>
-
+                {/* first mini flex */}
+                <div className="flex-col fjust-center falign-center pet-image-container">
+                    <img src="media/imgs/petphoto.jpg" alt='pet'>
+                    </img>
                 </div>
 
             </div>
@@ -74,40 +79,46 @@ const AdoptionAd = () => {
 
                 </div>
 
-            </div>
-
-            {/* third main flex item ++ check comments and leave a comment ++ */}
-
-            <div className="comments-area-wrapper flex-col falign-center gap-8p">
 
 
-                {/* wrapper of the comment text area and button to send comment */}
-                <div className="leave-comment-wrapper flex-col falign-center gap-16p">
-                    <textarea name="comment" id="comment" cols="30" rows="5" placeholder="feel free to share a comment!"></textarea>
-                    <input className="btn-rec-purple" type="submit" />
-                </div>
 
-
-                {/*wrapper of all the comments, I'm guessing we'll use pagination again :') */}
-                <div className="all-comments-wrapper flex-col gap-8p">
-
-
-                    {/* each single comment wrapper */}
-                    <div className="single-comment-wrapper flex-col gap-8p">
-
-                        {/* the username */}
-                        <div><i className="fas fa-user"></i>Maria</div>
-                        {/* user's comment */}
-                        <p>so cute! does mimo require any pet supplies or are they offered by you? </p>
-
-                    </div>
-
-                </div>
 
             </div>
 
+        
+
+            {/* third main flex item ++ check comments and leave a comment ++ */ }
+
+    <div className="comments-area-wrapper flex-col falign-center gap-8p">
+
+
+        {/* wrapper of the comment text area and button to send comment */}
+        <div className="leave-comment-wrapper flex-col falign-center gap-16p">
+            <textarea name="comment" id="comment" cols="30" rows="5" placeholder="feel free to share a comment!"></textarea>
+            <input className="btn-rec-purple" type="submit" />
+        </div>
+
+
+        {/*wrapper of all the comments, I'm guessing we'll use pagination again :') */}
+        <div className="all-comments-wrapper flex-col gap-8p">
+
+
+            {/* each single comment wrapper */}
+            <div className="single-comment-wrapper flex-col gap-8p">
+
+                {/* the username */}
+                <div><i className="fas fa-user"></i>Maria</div>
+                {/* user's comment */}
+                <p>so cute! does mimo require any pet supplies or are they offered by you? </p>
+
+            </div>
 
         </div>
+
+    </div>
+
+
+    </div>
     )
 }
 
