580b9130ab173a6722baca85c856c16dbf71d1ce
lagger94hub <ramisaadaldden@gmail.com>
2021-12-26 01:53:37 +0300

    finish adoption ads

--- website/pet-clinic-app/src/components/pages/adoptionAds/AdoptionAds.js
+++ website/pet-clinic-app/src/components/pages/adoptionAds/AdoptionAds.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 import { AnimatePresence, motion } from "framer-motion"
 import { adoptionMotion } from '../adoptionAds/adoptionMotion'
 import { useRef } from "react/cjs/react.development"
@@ -64,6 +64,8 @@ const serverFetch2 = () => {
 
 
 
+
+
 const AdoptionAds = () => {
 
 
@@ -72,45 +74,114 @@ const AdoptionAds = () => {
     posts: []
   })
   const isMounted = useRef(false)
+  const samePage = useRef(true)
 
-  //[{}, {}, {}]
-  // [false]
+  //setup the infinite scrolling
+  // we want to preserve the observer's values all the time that why we use ref
+  const observer = useRef()
 
-  // [true, {}, {}, {}]
+  // every time the last node get mounted the lastpost is changed to the new last node
+  const lastPost = useRef(null)
 
+  const observeLast = useCallback(() => {
+    // remove the attachment to the previous node so that scrolling up back to the previous last node wont fire a set
+    if (observer.current) observer.current.disconnect()
+
+    // create new attachment to the last node every thing written inside the arrow function will be called when there is an intersecting
+    observer.current = new IntersectionObserver((entries) => {
+
+      // if we are not intersecting with  the node or if loading is hapenning we do nothing
+      if (!entries[0].isIntersecting || postArr.isLoading) return
+
+
+      // When we touch the last node
+      setPostArr((oldObj) => {
+        return { ...oldObj, isLoading: true }
+      })
+
+    })
+
+    if (lastPost.current) observer.current.observe(lastPost.current)
+  }, [postArr.isLoading]);
+
+  // const observeLast = (lastPost) => {
+
+
+
+  //   // remove the attachment to the previous node so that scrolling up back to the previous last node wont fire a set
+  //   if (observer.current) observer.current.disconnect()
+
+  //   // create new attachment to the last node every thing written inside the arrow function will be called when there is an intersecting
+  //   observer.current = new IntersectionObserver((entries) => {
+
+  //     // if we are not intersecting with  the node or if loading is hapenning we do nothing
+  //     if (!entries[0].isIntersecting || postArr.isLoading) return
+
+
+  //     // When we touch the last node
+  //     setPostArr((oldObj) => {
+  //       return { ...oldObj, isLoading: true }
+  //     })
+
+  //   })
+
+  //   if (lastPost) observer.current.observe(lastPost)
+  // }
+
+
+  // what happens onload
   useEffect(() => {
     serverFetch1().then((result) => {
-      console.log('serverfetch1')
       setPostArr({ isLoading: false, posts: result })
     }).catch((error) => {
       console.log(error)
     })
   }, [])
 
+
+
+
+  //fetch from the database as the button gets pressed and update the posts array
   useEffect(() => {
+    observeLast()
     if (isMounted.current) {
       if (postArr.isLoading) {
-        serverFetch2().then((result) => {
-          console.log('serverfetch2')
-          setPostArr({ isLoading: false, posts: result })
-        }).catch((error) => {
-          console.log(error)
-        })
+        if (samePage.current) {
+          serverFetch2().then((result) => {
+            setPostArr((oldPosts) => {
+              return { isLoading: false, posts: [...oldPosts.posts, ...result] }
+            })
+          }).catch((error) => {
+            console.log(error)
+          })
+        } else {
+          serverFetch2().then((result) => {
+            samePage.current = true
+            setPostArr({ isLoading: false, posts: result })
+          }).catch((error) => {
+            console.log(error)
+          })
+        }
+
       }
     }
     else {
       isMounted.current = true
     }
-  }, [postArr])
+  }, [postArr, observeLast])
 
-
+  // find pets based of filters this will triggers the loading message by changing the loading state to true
   const findPets = () => {
-    if(!postArr.isLoading) {
+    if (!postArr.isLoading) {
+      samePage.current = false
       setPostArr((oldObj) => {
         return { ...oldObj, isLoading: true }
       })
     }
   }
+
+
+
 
   const itemMotion = adoptionMotion
 
@@ -190,38 +261,75 @@ const AdoptionAds = () => {
 
           <AnimatePresence exitBeforeEnter>
             {postArr.isLoading &&
-              <motion.div 
-              variants={itemMotion}
-              initial='initial'
-              animate='final'
-              exit='exit'
-              className="loaderWrapper flex-row fjust-center">
+              <motion.div
+                variants={itemMotion}
+                initial='initial'
+                animate='final'
+                exit='exit'
+                className="loaderWrapper-top flex-row fjust-center">
                 <div className="lds-ripple"><div></div><div></div></div>
               </motion.div>
             }
           </AnimatePresence>
           <AnimatePresence>
             {postArr.posts.map((post, index) => {
-              console.log('hello')
-              return (
-                <motion.a
-                  key={post.id}
-                  variants={itemMotion}
-                  initial='initial'
-                  animate='final'
-                  exit='exit'
-                  href="/#" className="adoption-post gap-8p flex-col falign-center ">
-                  <img src={post.img} alt="" className="post-image" />
-                  <p>{post.id}</p>
-                  <p><span>{post.type} - </span><span>{post.breed}</span></p>
-                  <p className="pTitle">{post.title}</p>
-                  <p><i className="fa fa-clock"></i>&nbsp;{post.createdAt}</p>
-                  <button href="/#" className="btn-rec-blue">
-                    View Post
-                  </button>
-                </motion.a>
-              )
-            })}
+              // mark the last post
+              if (postArr.posts.length === index + 1)
+                return (
+                  <motion.a
+                    key={post.id}
+                    variants={itemMotion}
+                    initial='initial'
+                    animate='final'
+                    exit='exit'
+                    href="/#"
+                    ref={lastPost}  //marking the last post
+                    className="adoption-post gap-8p flex-col falign-center ">
+                    <img src={post.img} alt="" className="post-image" />
+                    <p>{post.id}</p>
+                    <p><span>{post.type} - </span><span>{post.breed}</span></p>
+                    <p className="pTitle">{post.title}</p>
+                    <p><i className="fa fa-clock"></i>&nbsp;{post.createdAt}</p>
+                    <button href="/#" className="btn-rec-blue">
+                      View Post
+                    </button>
+                  </motion.a>
+                )
+              else
+                return (
+                  <motion.a
+                    key={post.id}
+                    variants={itemMotion}
+                    initial='initial'
+                    animate='final'
+                    exit='exit'
+                    href="/#"
+                    className="adoption-post gap-8p flex-col falign-center ">
+                    <img src={post.img} alt="" className="post-image" />
+                    <p>{post.id}</p>
+                    <p><span>{post.type} - </span><span>{post.breed}</span></p>
+                    <p className="pTitle">{post.title}</p>
+                    <p><i className="fa fa-clock"></i>&nbsp;{post.createdAt}</p>
+                    <button href="/#" className="btn-rec-blue">
+                      View Post
+                    </button>
+                  </motion.a>
+                )
+
+            }
+            ) }
+          </AnimatePresence>
+          <AnimatePresence exitBeforeEnter>
+            {postArr.isLoading &&
+              <motion.div
+                variants={itemMotion}
+                initial='initial'
+                animate='final'
+                exit='exit'
+                className="loaderWrapper flex-row fjust-center">
+                <div className="lds-ripple"><div></div><div></div></div>
+              </motion.div>
+            }
           </AnimatePresence>
 
 
