19dda1c30aa3681bde8774003da71e04c92fea1f
ramroming <reemalhalbouni@hotmail.com>
2022-02-18 11:54:25 +0300

    added registration and temprorary routing

--- website/pet-clinic-app/front-end/src/App.js
+++ website/pet-clinic-app/front-end/src/App.js
@@ -24,76 +24,76 @@ function App() {
   // creating the states that will be sent via the context
   const [token, setToken] = useState(false)
   const [userId, setUserId] = useState(false)
-  
+
   const login = useCallback((uid, token) => {
     setToken(token)
     setUserId(uid)
-    localStorage.setItem('userData', JSON.stringify({uid, token}))
+    localStorage.setItem('userData', JSON.stringify({ uid, token }))
   }, [])
 
-  const logout = useCallback( async () => {
+  const logout = useCallback(async () => {
 
     setToken(null)
     setUserId(null)
     localStorage.removeItem('userData')
-    
-    
+
+
   }, [])
 
   // to check if the user is logged in when refreshing the page this is done by checking the local storage to find user's data
   useEffect(() => {
-    const storedData =JSON.parse(localStorage.getItem('userData'))
+    const storedData = JSON.parse(localStorage.getItem('userData'))
     if (storedData && storedData.token) {
       login(storedData.uid, storedData.token)
     }
   }, [login])
-  
+
   // creating auth and nonauth routes
-  const routes = token ? (
-    <Routes>
-      <Route path='/' exact
-        element={<Home />}></Route>
-      <Route path='/home'
-        element={<Home />}></Route>
-      <Route path='/registerpet'
-        element={<RegisterPet />}></Route>
-      <Route path='/appointment'
-        element={<Appointment />}></Route>
-      <Route path='/adoption'
-        element={<Adoption />}></Route>
-      <Route path='/adoptionads'
-        element={<AdoptionAds />}></Route>
-      <Route path='/adoptionad'
-        element={<AdoptionAd />}></Route>
-      <Route path='/myprofile'
-        element={<Myprofile />}> </Route>
-      <Route path='/postad'
-        element={<PostAd />}> </Route>
-      <Route path='/postpreview'
-        element={<PostPreview />}> </Route>
-      <Route path='/stafflist'
-        element={<Stafflist />}> </Route>
-      <Route path='/staffpanel'
-        element={<StaffPanel />}> </Route>
-      <Route path='*'
-        element={<Navigate to='/' />}> </Route>
-    </Routes>
-  ) : (
-    <Routes>
-      <Route path='/' exact
-        element={<Home />}></Route>
-      <Route path='/home'
-        element={<Home />}></Route>
-      <Route path='/login'
-        element={<Login />}></Route>
-      <Route path='/signup'
-        element={<Signup />}></Route>
-      <Route path='/stafflist'
-        element={<Stafflist />}> </Route>
-      <Route path='*' 
-        element={<Navigate to='/login' />}></Route>
-    </Routes>
-  )
+  // const routes = token ? (
+  //   <Routes>
+  //     <Route path='/' exact
+  //       element={<Home />}></Route>
+  //     <Route path='/home'
+  //       element={<Home />}></Route>
+  //     <Route path='/registerpet'
+  //       element={<RegisterPet />}></Route>
+  //     <Route path='/appointment'
+  //       element={<Appointment />}></Route>
+  //     <Route path='/adoption'
+  //       element={<Adoption />}></Route>
+  //     <Route path='/adoptionads'
+  //       element={<AdoptionAds />}></Route>
+  //     <Route path='/adoptionad'
+  //       element={<AdoptionAd />}></Route>
+  //     <Route path='/myprofile'
+  //       element={<Myprofile />}> </Route>
+  //     <Route path='/postad'
+  //       element={<PostAd />}> </Route>
+  //     <Route path='/postpreview'
+  //       element={<PostPreview />}> </Route>
+  //     <Route path='/stafflist'
+  //       element={<Stafflist />}> </Route>
+  //     <Route path='/staffpanel'
+  //       element={<StaffPanel />}> </Route>
+  //     <Route path='*'
+  //       element={<Navigate to='/' />}> </Route>
+  //   </Routes>
+  // ) : (
+  //   <Routes>
+  //     <Route path='/' exact
+  //       element={<Home />}></Route>
+  //     <Route path='/home'
+  //       element={<Home />}></Route>
+  //     <Route path='/login'
+  //       element={<Login />}></Route>
+  //     <Route path='/signup'
+  //       element={<Signup />}></Route>
+  //     <Route path='/stafflist'
+  //       element={<Stafflist />}> </Route>
+  //     <Route path='*' 
+  //       element={<Navigate to='/login' />}></Route>
+  //   </Routes>
+  // )
   return (
     <>
       {/* wrapping all the components with our authcontext so that we can pass the data to the interested components when ever isloggedin changes all the components will be able to receive the change */}
@@ -101,7 +101,35 @@ function App() {
       <authContext.Provider value={{ isLoggedIn: !!token, userId, token, login, logout }}>
         <Nav />
         <ScrollToTop>
-          {routes}
+          {/* {routes} */}
+          <Routes>
+            <Route path='/' exact
+              element={<Home />}></Route>
+            <Route path='/home'
+              element={<Home />}></Route>
+            <Route path='/registerpet'
+              element={<RegisterPet />}></Route>
+            <Route path='/appointment'
+              element={<Appointment />}></Route>
+            <Route path='/adoption'
+              element={<Adoption />}></Route>
+            <Route path='/adoptionads'
+              element={<AdoptionAds />}></Route>
+            <Route path='/adoptionad'
+              element={<AdoptionAd />}></Route>
+            <Route path='/myprofile'
+              element={<Myprofile />}> </Route>
+            <Route path='/postad'
+              element={<PostAd />}> </Route>
+            <Route path='/postpreview'
+              element={<PostPreview />}> </Route>
+            <Route path='/stafflist'
+              element={<Stafflist />}> </Route>
+            <Route path='/staffpanel'
+              element={<StaffPanel />}> </Route>
+            <Route path='*'
+              element={<Navigate to='/' />}> </Route>
+          </Routes>
         </ScrollToTop>
         <Footer />
       </authContext.Provider>
--- website/pet-clinic-app/front-end/src/components/pages/staffPanel/staffPanelData/registration/Registration.js
+++ website/pet-clinic-app/front-end/src/components/pages/staffPanel/staffPanelData/registration/Registration.js
@@ -1,10 +1,15 @@
 
 
-
-
 const Registration = () => {
   return (
-    <div>Registration</div>
+    <div className="flex-col falign-center fjust-center gap-16p">
+        <button className="btn-rec">
+            Register a client
+        </button>
+        <button className="btn-rec">
+            Regsiter a pet
+        </button>
+    </div>
   )
 }
 
