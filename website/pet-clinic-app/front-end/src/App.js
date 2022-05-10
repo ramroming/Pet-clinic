d6d306e9c75ae9d95ee170ba70918f50ba96a458
lagger94hub <ramisaadaldden@gmail.com>
2022-05-10 15:01:14 +0300

    working on front end deployment

--- website/pet-clinic-app/front-end/src/App.js
+++ website/pet-clinic-app/front-end/src/App.js
@@ -1,10 +1,10 @@
 import { authContext } from "./components/shared/context/auth-context";
-import {pageLoadingContext} from "./components/shared/context/loading-context";
+import { pageLoadingContext } from "./components/shared/context/loading-context";
 import LoadingPage from './components/utils/loadingpage/LoadingPage';
 import ScrollToTop from "./components/utils/ScrollToTop";
 import useAuth from "./components/shared/hooks/auth-hook";
 import useRoutes from './components/shared/hooks/routes-hook';
-import { useState } from 'react';
+import { useState, Suspense } from 'react';
 
 
 
@@ -12,18 +12,23 @@ function App() {
 
   const { token, login, logout, userId, authedUser, userRole } = useAuth(window)
   const [pageIsLoading, setPageIsLoading] = useState(false)
- 
 
-  const routes =  useRoutes(authedUser, userRole)
+
+  const routes = useRoutes(authedUser, userRole)
   return (
     <>
       {/* wrapping all the components with our authcontext so that we can pass the data to the interested components when ever isloggedin changes all the components will be able to receive the change */}
       {/* here we use the doulbe exclamation mark to cast the token string to a boolean value  */}
-      <pageLoadingContext.Provider value={{pageIsLoading, setPageIsLoading}}>
+      <pageLoadingContext.Provider value={{ pageIsLoading, setPageIsLoading }}>
         <authContext.Provider value={{ isLoggedIn: !!token, userId, token, login, logout, userRole }}>
           <LoadingPage />
           <ScrollToTop>
-            {routes}
+            <Suspense fallback={<div className="loading-page">
+
+              <div className="lds-ripple-page"><div></div><div></div></div>
+            </div>}>
+              {routes}
+            </Suspense>
           </ScrollToTop>
         </authContext.Provider>
       </pageLoadingContext.Provider>
--- website/pet-clinic-app/front-end/src/components/shared/hooks/routes-hook.js
+++ website/pet-clinic-app/front-end/src/components/shared/hooks/routes-hook.js
@@ -1,38 +1,80 @@
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
-import Signup from "./../..//pages/signup/Signup";
-import Login from "./../..//pages/login/Login";
-import Home from "./../..//pages/home/Home";
-import RegisterPet from "./../..//pages/registerpet/RegisterPet";
-import Appointment from "./../..//pages/appointment/Appointment";
-import Adoption from "./../..//pages/adoption/Adoption";
-import AdoptionAds from "./../..//pages/adoptionAds/AdoptionAds";
-import AdoptionAd from "./../..//pages/adoptionAd/AdoptionAd";
-// import Myprofile from "./../..//pages/myprofile/Myprofile";
-import PostAd from "./../..//pages/postAd/PostAd";
-import PostPreview from "./../..//pages/postpreview/PostPreview";
-import Stafflist from "./../..//pages/stafflist/Stafflist";
-import About from "./../..//pages/about/About";
-import SideNav from "../../layout/profile_side_nav/SideNav";
-import Account from "../../pages/profile/account/Account";
-import MyAdoptionPosts from "../../pages/profile/myadoptionposts/MyAdoptionPosts";
-import MyAdoptionRequests from "../../pages/profile/myadoptionrequests/MyAdoptionRequests";
-import PersonalInfo from "../../pages/profile/personalinfo/PersonalInfo";
-import PetInfo from "../../pages/profile/petinfo/PetInfo";
-import StaffPanelSideNav from "../../layout/staffpanel_side_nav/StaffPanelSideNav";
-import AppointmentManagement from '../../pages/staffPanel/appointmentmanagement/AppointmentManagement';
-import AdoptionManagement from '../../pages/staffPanel/adoptionmanagement/AdoptionManagement';
-import AdoptionPostsManagement from '../../pages/staffPanel/adoptionpostsmanagement/AdoptionPostsManagement';
-import PetTreatmentHistory from '../../pages/staffPanel/pettreatmenthistory/PetTreatmentHistory';
-import PetTrainingHistory from '../../pages/staffPanel/pettraininghistory/PetTrainingHistory';
-import RegisterClient from '../../pages/staffPanel/registration/RegisterClient'
-import PutForAdoption from "../../pages/staffPanel/putforadoption/PutForAdoption";
-import RegisterPetRec from '../../pages/staffPanel/registration/RegisterPetRec'
-import ManageUsers from '../../pages/staffPanel/manageusers/ManageUsers'
-import ManagePets from '../../pages/staffPanel/managepets/ManagePets'
-import ManageProfits from '../../pages/staffPanel/manageProfits/ManageProfits'
-import ActiveAppointments from "../../pages/staffPanel/activeappointments/ActiveAppointments";
-import Nav from "../../layout/nav/Nav";
-import Footer from "../../layout/footer/Footer";
+import  React from 'react'
+
+// import Signup from "./../../pages/signup/Signup";
+// import Login from "./../../pages/login/Login";
+// import Home from "./../../pages/home/Home";
+// import RegisterPet from "./../../pages/registerpet/RegisterPet";
+// import Appointment from "./../../pages/appointment/Appointment";
+// import Adoption from "./../../pages/adoption/Adoption";
+// import AdoptionAds from "./../../pages/adoptionAds/AdoptionAds";
+// import AdoptionAd from "./../../pages/adoptionAd/AdoptionAd";
+// // import Myprofile from "./../..//pages/myprofile/Myprofile";
+// import PostAd from "./../../pages/postAd/PostAd";
+// import PostPreview from "./../../pages/postpreview/PostPreview";
+// import Stafflist from "./../../pages/stafflist/Stafflist";
+// import About from "./../../pages/about/About";
+// import SideNav from "../../layout/profile_side_nav/SideNav";
+// import Account from "../../pages/profile/account/Account";
+// import MyAdoptionPosts from "../../pages/profile/myadoptionposts/MyAdoptionPosts";
+// import MyAdoptionRequests from "../../pages/profile/myadoptionrequests/MyAdoptionRequests";
+// import PersonalInfo from "../../pages/profile/personalinfo/PersonalInfo";
+// import PetInfo from "../../pages/profile/petinfo/PetInfo";
+// import StaffPanelSideNav from "../../layout/staffpanel_side_nav/StaffPanelSideNav";
+// import AppointmentManagement from '../../pages/staffPanel/appointmentmanagement/AppointmentManagement';
+// import AdoptionManagement from '../../pages/staffPanel/adoptionmanagement/AdoptionManagement';
+// import AdoptionPostsManagement from '../../pages/staffPanel/adoptionpostsmanagement/AdoptionPostsManagement';
+// import PetTreatmentHistory from '../../pages/staffPanel/pettreatmenthistory/PetTreatmentHistory';
+// import PetTrainingHistory from '../../pages/staffPanel/pettraininghistory/PetTrainingHistory';
+// import RegisterClient from '../../pages/staffPanel/registration/RegisterClient'
+// import PutForAdoption from "../../pages/staffPanel/putforadoption/PutForAdoption";
+// import RegisterPetRec from '../../pages/staffPanel/registration/RegisterPetRec'
+// import ManageUsers from '../../pages/staffPanel/manageusers/ManageUsers'
+// import ManagePets from '../../pages/staffPanel/managepets/ManagePets'
+// import ManageProfits from '../../pages/staffPanel/manageProfits/ManageProfits'
+// import ActiveAppointments from "../../pages/staffPanel/activeappointments/ActiveAppointments";
+// import Nav from "../../layout/nav/Nav";
+// import Footer from "../../layout/footer/Footer";
+
+
+const Signup = React.lazy(() => import('./../..//pages/signup/Signup'))
+const Login = React.lazy(() => import('./../../pages/login/Login'))
+const Home = React.lazy(() => import('./../../pages/home/Home'))
+const RegisterPet = React.lazy(() => import('./../../pages/registerpet/RegisterPet'))
+const Appointment = React.lazy(() => import('./../../pages/appointment/Appointment'))
+const Adoption = React.lazy(() => import('./../../pages/adoption/Adoption'))
+const AdoptionAds = React.lazy(() => import('./../../pages/adoptionAds/AdoptionAds'))
+const AdoptionAd = React.lazy(() => import('./../../pages/adoptionAd/AdoptionAd'))
+const PostAd = React.lazy(() => import('./../../pages/postAd/PostAd'))
+const PostPreview = React.lazy(() => import('./../../pages/postpreview/PostPreview'))
+const Stafflist = React.lazy(() => import('./../../pages/stafflist/Stafflist'))
+const About = React.lazy(() => import('./../../pages/about/About'))
+const SideNav = React.lazy(() => import('../../layout/profile_side_nav/SideNav'))
+const Account = React.lazy(() => import('../../pages/profile/account/Account'))
+const MyAdoptionPosts = React.lazy(() => import('../../pages/profile/myadoptionposts/MyAdoptionPosts'))
+const MyAdoptionRequests = React.lazy(() => import('../../pages/profile/myadoptionrequests/MyAdoptionRequests'))
+const PersonalInfo = React.lazy(() => import('../../pages/profile/personalinfo/PersonalInfo'))
+const PetInfo = React.lazy(() => import('../../pages/profile/petinfo/PetInfo'))
+const StaffPanelSideNav = React.lazy(() => import('../../layout/staffpanel_side_nav/StaffPanelSideNav'))
+const AppointmentManagement = React.lazy(() => import('../../pages/staffPanel/appointmentmanagement/AppointmentManagement'))
+const AdoptionManagement = React.lazy(() => import('../../pages/staffPanel/adoptionmanagement/AdoptionManagement'))
+const AdoptionPostsManagement = React.lazy(() => import('../../pages/staffPanel/adoptionpostsmanagement/AdoptionPostsManagement'))
+const PetTreatmentHistory = React.lazy(() => import('../../pages/staffPanel/pettreatmenthistory/PetTreatmentHistory'))
+const PetTrainingHistory = React.lazy(() => import('../../pages/staffPanel/pettraininghistory/PetTrainingHistory'))
+const RegisterClient = React.lazy(() => import('../../pages/staffPanel/registration/RegisterClient'))
+const PutForAdoption = React.lazy(() => import('../../pages/staffPanel/putforadoption/PutForAdoption'))
+const RegisterPetRec = React.lazy(() => import('../../pages/staffPanel/registration/RegisterPetRec'))
+const ManageUsers = React.lazy(() => import('../../pages/staffPanel/manageusers/ManageUsers'))
+const ManagePets = React.lazy(() => import('../../pages/staffPanel/managepets/ManagePets'))
+const ManageProfits = React.lazy(() => import('../../pages/staffPanel/manageProfits/ManageProfits'))
+const ActiveAppointments = React.lazy(() => import('../../pages/staffPanel/activeappointments/ActiveAppointments'))
+const Nav = React.lazy(() => import('../../layout/nav/Nav'))
+const Footer = React.lazy(() => import('../../layout/footer/Footer'))
+
+
+
+
+
 // A hook that contains the routing logic and it uses the authed user state to determine whether the user is allowed to navigate to private Routes
 const useRoutes = (authedUser, stmemType) => {
 
